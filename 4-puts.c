486ee35f34b665697787c427a4f7829a5ce27db9
Zee363 <zinhle.nhlapo31@gmail.com>
2023-06-19 08:48:47 -0700

    Hello,puts

--- 4-puts.c
+++ 4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Enrty Point
+ *Description:main - A program that prints out a line using puts function
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-}	
+}
