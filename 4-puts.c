0334662b04e15b31c077081a28afc3fc14d8fbd1
Zee363 <zinhle.nhlapo31@gmail.com>
2023-06-20 05:08:43 -0700

    task 4

--- 4-puts.c
+++ 4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *Description:main - A program that prints out a line using puts function
+ * main - Aprogram that prints a line with the puts function
  *
- *Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
