a918d7eb463f6a745c2c747987d070ef020d4f65
Zee363 <zinhle.nhlapo31@gmail.com>
2023-06-21 05:14:31 -0700

    I sometimes suffer from insomia.And when I can't sleep,I play what the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,19 @@
 int main(void)
 {
 	int ch = 'a';
+	int CH = 'A';
 
-	while (ch <= '2')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
 	putchar('\n');
 	return (0);
 }
