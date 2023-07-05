4aa086e087859faed3ce40254c5327d1219aca9c
Zee363 <zinhle.nhlapo31@gmail.com>
2023-07-05 10:04:46 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,13 +2,14 @@
 
 /**
  * print_chessboard - a function that prints the chessboard
- * @a: pointer
- * Return: void
+ * @a: array
+ * Return: Always 0 (Success)
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int c, d;
+	int c; 
+	int d;
 
 	for (c = 0; c < 8; c++)
 	{
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,14 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - a function that prints the sum of the two diagonals of a square matrix of integers
- * @a: the matrix of integers
- * @size: the size of the matrix
+ * print_diagsums - a function that prints the sum
+ * of the two diagonals of a square matrix of integers
+ * @a: input
+ * @size: Always 0 (Success)
  */
 
 void print_diagsums(int *a, int size)
 {
-	int index, sum1 = 0, sum2 = 0;
+	int sum1, sum2, y;
 
-	for (index = 0; index < size; index++)
-	{ sum1 += s
+	sum1 = 0;
+	sum2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum2 + a[y * size + y];
+	}
+
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
